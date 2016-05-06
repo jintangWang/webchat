8c071084eaadde6b63aa63d6a82be3fb2154fefd
王金堂 <LCX6916356@163.com>
2016-05-06 16:32:45 +0800

    字母索引fix

--- www/templates/setting/setting.controller.js
+++ www/templates/setting/setting.controller.js
@@ -28,22 +28,45 @@ angular.module('wechat.controllers')
          $state.go('area'); 
     }
 })
-.controller('areaCtrl', function($scope,AreaListFactory,LetterFactory,$window){
+.controller('areaCtrl', function($scope,AreaListFactory,LetterFactory,$window,
+    $filter,$ionicScrollDelegate){
 
     AreaListFactory.getAreaList().then(function (callback) {
         if(callback.status==200){
-            $scope.areas=callback.data.result;
+            $scope.areas=$filter('orderBy')(callback.data.result,$scope.areaOrder);
             $scope.areaLetters=[];
             for (var i = 0; i < $scope.areas.length; i++) {
                 $scope.areaLetters.push(LetterFactory.getLetter($scope.areas[i].region_name).toString());
             }
+            $scope.index=$scope.letters.indexOf(LetterFactory.getLetter($scope.areas[0].region_name)[0]);
+            itemHeight=$("#area ul li").outerHeight();
+            
         } 
     })
 
-    $window.onscroll=function () {
-         console.log(1); 
-    }
 
+//     angular.element($('#area')).on('scroll',function(){
+//         $("#area ul").scroll();
+//         console.log(1)
+//     })
+    var itemHeight;
+    $scope.scroll=function(){
+        itemHeight=itemHeight||$("#area ul li").outerHeight();
+        var top=$ionicScrollDelegate.$getByHandle('area').getScrollPosition().top;
+        var index=Math.floor(top/itemHeight);
+         $scope.index=$scope.letters.indexOf(LetterFactory.getLetter($scope.areas[index].region_name)[0]);
+         $scope.$apply();
+        // console.log('滚动的距离='+top);
+    }    
+
+    //点击字母索引事件
+    $scope.selectItem=function (event) {
+        //获取offsetHeight的操作放在事件中,其他地方取不到的奇怪bug
+        itemHeight=itemHeight||$("#area ul li").outerHeight();
+        var target=event.target.innerText;
+        var selIdx= $scope.areaLetters.indexOf(target);
+        $ionicScrollDelegate.$getByHandle('area').scrollTo(0,selIdx*itemHeight);
+    }
 
     $scope.letters=['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 
@@ -51,6 +74,6 @@ angular.module('wechat.controllers')
     $scope.areaOrder=function (input) {
           return LetterFactory.getLetter(input.region_name);
     }
-
+    
     
 })
